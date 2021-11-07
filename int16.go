a7031552e365d4f75d2af132cde8862f41e7f4c1
r-fujiyama <hujiyama2843@gmail.com>
2021-11-07 11:39:56 +0000

    add int16

---
+++ int16.go
@@ -0,0 +1,90 @@
+package nulltype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"unsafe"
+)
+
+// Int16 represents a int16 that may be null.
+type Int16 struct {
+	Int16 int16
+	Valid bool
+}
+
+// NewInt16 creates an instance of Int16.
+func NewInt16(i16 int16, valid bool) Int16 {
+	return Int16{Int16: i16, Valid: valid}
+}
+
+// Scan implements the Scanner interface.
+func (i *Int16) Scan(value interface{}) error {
+	if value == nil {
+		i.Int16, i.Valid = 0, false
+		return nil
+	}
+
+	i.Valid = true
+	switch data := value.(type) {
+	case string:
+		i16, err := strconv.ParseInt(data, 10, 16)
+		if err != nil {
+			return err
+		}
+		i.Int16 = int16(i16)
+		return nil
+	case []byte:
+		i16, err := strconv.ParseInt(*(*string)(unsafe.Pointer(&data)), 10, 16)
+		if err != nil {
+			return err
+		}
+		i.Int16 = int16(i16)
+		return nil
+	case int:
+		i.Int16 = int16(data)
+		return nil
+	case int16:
+		i.Int16 = int16(data)
+		return nil
+	default:
+		return fmt.Errorf("got data of type %T", value)
+	}
+}
+
+// Value implements the driver Valuer interface.
+func (i *Int16) Value() (driver.Value, error) {
+	if !i.Valid {
+		return nil, nil
+	}
+	return i.Int16, nil
+}
+
+// MarshalJSON encode the value to JSON.
+func (i Int16) MarshalJSON() ([]byte, error) {
+	if !i.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(i.Int16)
+}
+
+// UnmarshalJSON decode data to the value.
+func (i *Int16) UnmarshalJSON(data []byte) error {
+	var fl *int16
+	if err := json.Unmarshal(data, &fl); err != nil {
+		return err
+	}
+	i.Valid = fl != nil
+	if fl == nil {
+		i.Int16 = 0
+	} else {
+		i.Int16 = *fl
+	}
+	return nil
+}
+
+// IsZeroOrEmpty return true if int16 is 0 or Valid is false.
+func (i *Int16) IsZeroOrNull() bool {
+	return i.Int16 == 0 || !i.Valid
+}
---
+++ int16_test.go
@@ -0,0 +1,236 @@
+package nulltype
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+)
+
+func TestInt16NewInt16(t *testing.T) {
+	val := NewInt16(1, true)
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanNull(t *testing.T) {
+	val := Int16{}
+	if err := val.Scan(nil); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 0, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanString(t *testing.T) {
+	val := Int16{}
+	if err := val.Scan("1"); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanByte(t *testing.T) {
+	val := Int16{}
+	if err := val.Scan([]byte("1")); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanInt(t *testing.T) {
+	val := Int16{}
+	var i int = 1
+	if err := val.Scan(i); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanInt16(t *testing.T) {
+	val := Int16{}
+	var i16 int16 = 1
+	if err := val.Scan(i16); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16ScanStringParseError(t *testing.T) {
+	val := Int16{}
+	err := val.Scan("foo")
+	if err == nil {
+		t.Fatalf("no error is output")
+	}
+}
+
+func TestInt16ScanStringByteError(t *testing.T) {
+	val := Int16{}
+	err := val.Scan([]byte("foo"))
+	if err == nil {
+		t.Fatalf("no error is output")
+	}
+}
+
+func TestInt16ScanTypeError(t *testing.T) {
+	val := Int16{}
+	err := val.Scan(struct{}{})
+	if err == nil || err.Error() != "got data of type struct {}" {
+		t.Fatalf("want %v, but %v:", "got data of type struct {}", err)
+	}
+}
+
+func TestInt16ValueInt(t *testing.T) {
+	val := Int16{Int16: 1, Valid: true}
+	got, err := val.Value()
+	if got != int16(1) || err != nil {
+		t.Fatalf("want %v, but %v:", "1", got)
+	}
+}
+
+func TestInt16ValueZero(t *testing.T) {
+	val := Int16{Int16: 0, Valid: true}
+	got, err := val.Value()
+	if got != int16(0) || err != nil {
+		t.Fatalf("want %v, but %v:", 0, got)
+	}
+}
+
+func TestInt16ValueNull(t *testing.T) {
+	val := Int16{Int16: 0, Valid: false}
+	got, err := val.Value()
+	if got != nil || err != nil {
+		t.Fatalf("want %v, but %v:", 0, got)
+	}
+}
+
+func TestInt16MarshalJSONInt(t *testing.T) {
+	val := Int16{Int16: 1, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "1"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestInt16MarshalJSONZero(t *testing.T) {
+	val := Int16{Int16: 0, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "0"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestInt16MarshalJSONNull(t *testing.T) {
+	val := Int16{Int16: 0, Valid: false}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "null"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestInt16UnmarshalJSONInt(t *testing.T) {
+	var val Int16
+	err := json.NewDecoder(strings.NewReader("1")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16UnmarshalJSONZero(t *testing.T) {
+	var val Int16
+	err := json.NewDecoder(strings.NewReader("0")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 0, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16UnmarshalJSONNull(t *testing.T) {
+	var val Int16
+	err := json.NewDecoder(strings.NewReader("null")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Int16{Int16: 0, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestInt16UnmarshalJSONError(t *testing.T) {
+	val := Int16{}
+	err := val.UnmarshalJSON([]byte("foo"))
+	if err == nil {
+		t.Fatal("no error message is output")
+	}
+}
+
+func TestInt16isZeroOrNullInt(t *testing.T) {
+	val := Int16{Int16: 1, Valid: true}
+	if val.IsZeroOrNull() {
+		t.Fatal("should not be zero or null")
+	}
+}
+
+func TestInt16isZeroOrNullZero(t *testing.T) {
+	val := Int16{Int16: 0, Valid: true}
+	if !val.IsZeroOrNull() {
+		t.Fatal("it has to be zero or null")
+	}
+}
+
+func TestInt16isZeroOrNullNull(t *testing.T) {
+	val := Int16{Int16: 0, Valid: false}
+	if !val.IsZeroOrNull() {
+		t.Fatal("it has to be zero or null")
+	}
+}
