5f57038260969109895dfafbb3a356798d2db826
r-fujiyama <hujiyama2843@gmail.com>
2021-11-07 15:00:22 +0000

    add time

---
+++ time.go
@@ -0,0 +1,75 @@
+package nulltype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"time"
+)
+
+// Time represents a bool that may be null.
+type Time struct {
+	Time  time.Time
+	Valid bool
+}
+
+// NewTime creates an instance of Time.
+func NewTime(t time.Time, valid bool) Time {
+	return Time{Time: t, Valid: valid}
+}
+
+// Scan implements the Scanner interface.
+func (t *Time) Scan(value interface{}) error {
+	if value == nil {
+		t.Time, t.Valid = time.Time{}, false
+		return nil
+	}
+
+	t.Valid = true
+	switch data := value.(type) {
+	case time.Time:
+		t.Time = data
+		return nil
+	default:
+		return fmt.Errorf("got data of type %T", value)
+	}
+}
+
+// Value implements the driver Valuer interface.
+func (t Time) Value() (driver.Value, error) {
+	if !t.Valid {
+		return nil, nil
+	}
+	return t.Time, nil
+}
+
+// MarshalJSON encode the value to JSON.
+func (t Time) MarshalJSON() ([]byte, error) {
+	if !t.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(t.Time)
+}
+
+// UnmarshalJSON decode data to the value.
+func (t *Time) UnmarshalJSON(data []byte) error {
+	var tt *time.Time
+	if err := json.Unmarshal(data, &tt); err != nil {
+		return err
+	}
+	t.Valid = tt != nil
+	if tt == nil {
+		t.Time = time.Time{}
+	} else {
+		t.Time = *tt
+	}
+	return nil
+}
+
+// String return string indicated the value.
+func (t Time) String() string {
+	if !t.Valid {
+		return "<null>"
+	}
+	return t.Time.String()
+}
---
+++ time_test.go
@@ -0,0 +1,145 @@
+package nulltype
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+	"time"
+)
+
+var testTime time.Time = time.Date(2022, 12, 31, 23, 59, 59, 0, time.UTC)
+
+func TestTimeNewTime(t *testing.T) {
+	val := NewTime(testTime, true)
+	want := Time{Time: testTime, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestTimeScanNull(t *testing.T) {
+	val := Time{}
+	if err := val.Scan(nil); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Time{Time: time.Time{}, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestTimeScanTime(t *testing.T) {
+	val := Time{}
+	if err := val.Scan(testTime); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Time{Time: testTime, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestTimeScanTypeError(t *testing.T) {
+	val := Time{}
+	err := val.Scan(struct{}{})
+	if err == nil || err.Error() != "got data of type struct {}" {
+		t.Fatalf("want %v, but %v:", "got data of type struct {}", err)
+	}
+}
+
+func TestTimeValueTime(t *testing.T) {
+	val := Time{Time: testTime, Valid: true}
+	got, err := val.Value()
+	if got != testTime || err != nil {
+		t.Fatalf("want %v, but %v:", true, got)
+	}
+}
+
+func TestTimeValueNull(t *testing.T) {
+	val := Time{Time: time.Time{}, Valid: false}
+	got, err := val.Value()
+	if got != nil || err != nil {
+		t.Fatalf("want %v, but %v:", "", got)
+	}
+}
+
+func TestTimeMarshalJSONTime(t *testing.T) {
+	val := Time{Time: testTime, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := `"2022-12-31T23:59:59Z"`
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestTimeMarshalJSONNull(t *testing.T) {
+	val := Time{Time: time.Time{}, Valid: false}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "null"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestTimeUnmarshalJSONTime(t *testing.T) {
+	var val Time
+	err := json.NewDecoder(strings.NewReader(`"2022-12-31T23:59:59Z"`)).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Time{Time: testTime, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestTimeUnmarshalJSONNull(t *testing.T) {
+	var val Time
+	err := json.NewDecoder(strings.NewReader("null")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Time{Time: time.Time{}, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestTimeUnmarshalJSONError(t *testing.T) {
+	val := Time{}
+	err := val.UnmarshalJSON([]byte("foo"))
+	if err == nil {
+		t.Fatal("no error message is output")
+	}
+}
+
+func TestTimeString(t *testing.T) {
+	val := Time{Time: testTime, Valid: true}
+	want := "2022-12-31 23:59:59 +0000 UTC"
+	got := val.String()
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+
+	val = Time{Time: time.Time{}, Valid: false}
+	want = "<null>"
+	got = val.String()
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
