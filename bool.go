bd31f24c0ce1864cad2e532df09303949184a498
r-fujiyama <hujiyama2843@gmail.com>
2021-11-07 12:41:38 +0000

    add bool

---
+++ bool.go
@@ -0,0 +1,94 @@
+package nulltype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"unsafe"
+)
+
+// Bool represents a bool that may be null.
+type Bool struct {
+	Bool  bool
+	Valid bool
+}
+
+// NewBool creates an instance of Bool.
+func NewBool(b bool, valid bool) Bool {
+	return Bool{Bool: b, Valid: valid}
+}
+
+// Scan implements the Scanner interface.
+func (b *Bool) Scan(value interface{}) error {
+	if value == nil {
+		b.Bool, b.Valid = false, false
+		return nil
+	}
+
+	b.Valid = true
+	switch data := value.(type) {
+	case string:
+		toBool, err := strconv.ParseBool(data)
+		if err != nil {
+			return err
+		}
+		b.Bool = toBool
+		return nil
+	case []byte:
+		toBool, err := strconv.ParseBool(*(*string)(unsafe.Pointer(&data)))
+		if err != nil {
+			return err
+		}
+		b.Bool = toBool
+		return nil
+	case int:
+		b.Bool = data == 1
+		return nil
+	case int16:
+		b.Bool = data == 1
+		return nil
+	case int32:
+		b.Bool = data == 1
+		return nil
+	case int64:
+		b.Bool = data == 1
+		return nil
+	case bool:
+		b.Bool = data
+		return nil
+	default:
+		return fmt.Errorf("got data of type %T", value)
+	}
+}
+
+// Value implements the driver Valuer interface.
+func (b *Bool) Value() (driver.Value, error) {
+	if !b.Valid {
+		return nil, nil
+	}
+	return b.Bool, nil
+}
+
+// MarshalJSON encode the value to JSON.
+func (b Bool) MarshalJSON() ([]byte, error) {
+	if !b.Valid {
+		return []byte("null"), nil
+	}
+	return json.Marshal(b.Bool)
+}
+
+// UnmarshalJSON decode data to the value.
+func (b *Bool) UnmarshalJSON(data []byte) error {
+	var bb *bool
+	if err := json.Unmarshal(data, &bb); err != nil {
+		return err
+	}
+	b.Valid = bb != nil
+	if bb == nil {
+		b.Bool = false
+	} else {
+		b.Bool = *bb
+	}
+	return nil
+}
---
+++ bool_test.go
@@ -0,0 +1,220 @@
+package nulltype
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
+	"testing"
+)
+
+func TestBoolNewBool(t *testing.T) {
+	val := NewBool(true, true)
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+
+	toBool, _ := strconv.ParseBool("asefawe")
+	fmt.Println(toBool) // true
+	b := []byte("0")
+	t.Fatal(b)
+}
+
+func TestBoolScanNull(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(nil); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: false, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanString(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan("true"); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanByte(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan([]byte("true")); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanInt(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(1); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanInt16(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(int16(1)); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanInt32(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(int32(1)); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+func TestBoolScanInt64(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(int64(1)); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanBool(t *testing.T) {
+	val := Bool{}
+	if err := val.Scan(true); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolScanStringParseError(t *testing.T) {
+	val := Bool{}
+	err := val.Scan("parse error")
+	if err == nil {
+		t.Fatalf("no parse error is output")
+	}
+}
+
+func TestBoolScanByteParseError(t *testing.T) {
+	val := Bool{}
+	err := val.Scan([]byte("parse error"))
+	if err == nil {
+		t.Fatalf("no parse error is output")
+	}
+}
+
+func TestBoolScanTypeError(t *testing.T) {
+	val := Bool{}
+	err := val.Scan(struct{}{})
+	if err == nil || err.Error() != "got data of type struct {}" {
+		t.Fatalf("want %v, but %v:", "got data of type struct {}", err)
+	}
+}
+
+func TestBoolValueBool(t *testing.T) {
+	val := Bool{Bool: true, Valid: true}
+	got, err := val.Value()
+	if got != true || err != nil {
+		t.Fatalf("want %v, but %v:", true, got)
+	}
+}
+
+func TestBoolValueNull(t *testing.T) {
+	val := Bool{Bool: false, Valid: false}
+	got, err := val.Value()
+	if got != nil || err != nil {
+		t.Fatalf("want %v, but %v:", "", got)
+	}
+}
+
+func TestBoolMarshalJSONBool(t *testing.T) {
+	val := Bool{Bool: true, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "true"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestBoolMarshalJSONNull(t *testing.T) {
+	val := Bool{Bool: false, Valid: false}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "null"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestBoolUnmarshalJSONBool(t *testing.T) {
+	var val Bool
+	err := json.NewDecoder(strings.NewReader("true")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: true, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolUnmarshalJSONNull(t *testing.T) {
+	var val Bool
+	err := json.NewDecoder(strings.NewReader("null")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Bool{Bool: false, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestBoolUnmarshalJSONError(t *testing.T) {
+	val := Bool{}
+	err := val.UnmarshalJSON([]byte("foo"))
+	if err == nil {
+		t.Fatal("no error message is output")
+	}
+}
