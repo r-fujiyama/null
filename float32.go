260ce996247910686f6aba3a72c5930e504e8203
r-fujiyama <hujiyama2843@gmail.com>
2022-10-02 05:52:36 +0000

    add float32 type

---
+++ float32.go
@@ -0,0 +1,96 @@
+package nulltype
+
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"fmt"
+	"strconv"
+)
+
+// Float32 represents a float32 that may be null.
+type Float32 struct {
+	Float32 float32
+	Valid   bool
+}
+
+// Scan implements the Scanner interface.
+func (f *Float32) Scan(value interface{}) error {
+	if value == nil {
+		f.Float32, f.Valid = 0, false
+		return nil
+	}
+
+	f.Valid = true
+	switch data := value.(type) {
+	case string:
+		f32, err := strconv.ParseFloat(data, 32)
+		if err != nil {
+			return err
+		}
+		f.Float32 = float32(f32)
+		return nil
+	case []byte:
+		f32, err := strconv.ParseFloat(string(data), 32)
+		if err != nil {
+			return err
+		}
+		f.Float32 = float32(f32)
+		return nil
+	case int:
+		f.Float32 = float32(data)
+		return nil
+	case int8:
+		f.Float32 = float32(data)
+		return nil
+	case int16:
+		f.Float32 = float32(data)
+		return nil
+	case int32:
+		f.Float32 = float32(data)
+		return nil
+	case int64:
+		f.Float32 = float32(data)
+		return nil
+	case float32:
+		f.Float32 = data
+		return nil
+	default:
+		return fmt.Errorf("unsupported type: %T", value)
+	}
+}
+
+// Value implements the driver Valuer interface.
+func (f Float32) Value() (driver.Value, error) {
+	if !f.Valid {
+		return nil, nil
+	}
+	return f.Float32, nil
+}
+
+// MarshalJSON encode the value to JSON.
+func (f Float32) MarshalJSON() ([]byte, error) {
+	if !f.Valid {
+		return []byte("null"), nil
+	}
+	return JSONMarshal(f.Float32)
+}
+
+// UnmarshalJSON decode data to the value.
+func (f *Float32) UnmarshalJSON(data []byte) error {
+	var f32 *float32
+	if err := json.Unmarshal(data, &f32); err != nil {
+		return err
+	}
+	f.Valid = f32 != nil
+	if f32 == nil {
+		f.Float32 = 0
+	} else {
+		f.Float32 = *f32
+	}
+	return nil
+}
+
+// IsZeroOrNull return true if float32 is 0 or Valid is false.
+func (f *Float32) IsZeroOrNull() bool {
+	return f.Float32 == 0 || !f.Valid
+}
---
+++ float32_test.go
@@ -0,0 +1,280 @@
+package nulltype
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"testing"
+)
+
+func TestFloat32ScanNull(t *testing.T) {
+	val := Float32{}
+	if err := val.Scan(nil); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 0, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanString(t *testing.T) {
+	val := Float32{}
+	if err := val.Scan("1.1"); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1.1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanByte(t *testing.T) {
+	val := Float32{}
+	if err := val.Scan([]byte("1.1")); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1.1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanInt(t *testing.T) {
+	val := Float32{}
+	var i int = 1
+	if err := val.Scan(i); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanInt8(t *testing.T) {
+	val := Float32{}
+	var i8 int8 = 1
+	if err := val.Scan(i8); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanInt16(t *testing.T) {
+	val := Float32{}
+	var i16 int16 = 1
+	if err := val.Scan(i16); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanInt32(t *testing.T) {
+	val := Float32{}
+	var i32 int32 = 1
+	if err := val.Scan(i32); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanInt64(t *testing.T) {
+	val := Float32{}
+	var i64 int64 = 1
+	if err := val.Scan(i64); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanFloat32(t *testing.T) {
+	val := Float32{}
+	var f32 float32 = 1.1
+	if err := val.Scan(f32); err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1.1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32ScanStringParseError(t *testing.T) {
+	val := Float32{}
+	err := val.Scan("foo")
+	if err == nil {
+		t.Fatalf("no error is output")
+	}
+}
+
+func TestFloat32ScanStringByteError(t *testing.T) {
+	val := Float32{}
+	err := val.Scan([]byte("foo"))
+	if err == nil {
+		t.Fatalf("no error is output")
+	}
+}
+
+func TestFloat32ScanTypeError(t *testing.T) {
+	val := Float32{}
+	err := val.Scan(struct{}{})
+	if err == nil || err.Error() != "unsupported type: struct {}" {
+		t.Fatalf("want %v, but %v:", "unsupported type: struct {}", err)
+	}
+}
+
+func TestFloat32ValueFloat(t *testing.T) {
+	val := Float32{Float32: 1.1, Valid: true}
+	got, err := val.Value()
+	if got != float32(1.1) || err != nil {
+		t.Fatalf("want %v, but %v:", 1.1, got)
+	}
+}
+
+func TestFloat32ValueZero(t *testing.T) {
+	val := Float32{Float32: 0, Valid: true}
+	got, err := val.Value()
+	if got != float32(0.0) || err != nil {
+		t.Fatalf("want %v, but %v:", 0.0, got)
+	}
+}
+
+func TestFloat32ValueNull(t *testing.T) {
+	val := Float32{Float32: 0, Valid: false}
+	got, err := val.Value()
+	if got != nil || err != nil {
+		t.Fatalf("want %v, but %v:", 0.0, got)
+	}
+}
+
+func TestFloat32MarshalJSONFloat(t *testing.T) {
+	val := Float32{Float32: 1.1, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "1.1"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestFloat32MarshalJSONZero(t *testing.T) {
+	val := Float32{Float32: 0, Valid: true}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "0"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestFloat32MarshalJSONNull(t *testing.T) {
+	val := Float32{Float32: 0, Valid: false}
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(val); err != nil {
+		t.Fatal(err)
+	}
+
+	want := "null"
+	got := strings.TrimSpace(buf.String())
+	if got != want {
+		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func TestFloat32UnmarshalJSONFloat(t *testing.T) {
+	var val Float32
+	err := json.NewDecoder(strings.NewReader("1.1")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 1.1, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32UnmarshalJSONZero(t *testing.T) {
+	var val Float32
+	err := json.NewDecoder(strings.NewReader("0")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 0, Valid: true}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32UnmarshalJSONNull(t *testing.T) {
+	var val Float32
+	err := json.NewDecoder(strings.NewReader("null")).Decode(&val)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	want := Float32{Float32: 0, Valid: false}
+	if val != want {
+		t.Fatalf("want %v, but %v:", want, val)
+	}
+}
+
+func TestFloat32UnmarshalJSONError(t *testing.T) {
+	val := Float32{}
+	err := val.UnmarshalJSON([]byte("foo"))
+	if err == nil {
+		t.Fatal("no error message is output")
+	}
+}
+
+func TestFloat32isZeroOrNullFloat(t *testing.T) {
+	val := Float32{Float32: 1.1, Valid: true}
+	if val.IsZeroOrNull() {
+		t.Fatal("should not be zero or null")
+	}
+}
+
+func TestFloat32isZeroOrNullZero(t *testing.T) {
+	val := Float32{Float32: 0, Valid: true}
+	if !val.IsZeroOrNull() {
+		t.Fatal("it has to be zero or null")
+	}
+}
+
+func TestFloat32isZeroOrNullNull(t *testing.T) {
+	val := Float32{Float32: 0, Valid: false}
+	if !val.IsZeroOrNull() {
+		t.Fatal("it has to be zero or null")
+	}
+}
